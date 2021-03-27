ff29ba8d57817b7dcc067fdb4555eecf0678445c
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-27 14:45:14 -0300

    update python script
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- run.py
+++ run.py
@@ -9,10 +9,15 @@ print(f"💡 \033[36mExample: Getting Brazil Covid-19 datas\033[0m")
 
 response = requests.get("https://covid-api.mmediagroup.fr/v1/cases?country=Brazil")
 
-datas = response.json()
+if response.status == 200:
+    datas = response.json()
 
-country_summary = datas["All"]
+    country_summary = datas["All"]
 
-print("🤒 Confirmed cases:", country_summary["confirmed"])
-print("🥳 Recovered cases:", country_summary["recovered"])
-print("😢 Deaths:", country_summary["deaths"])
+    print("🤒 Confirmed cases:", country_summary["confirmed"])
+    print("🥳 Recovered cases:", country_summary["recovered"])
+    print("😢 Deaths:", country_summary["deaths"])
+
+else:
+    print("❌ Couldn't get Brazil Covid-19 datas")
+    print (response.status_code, response.reason)
