1321b401234d00fda6f365da59574a1d1a151f00
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-27 13:58:39 -0300

    update python script
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- run.py
+++ run.py
@@ -1,2 +1,24 @@
-print("Hello World!")
+import requests
+import json
+
 print("You can add your script rules here to execute them with the GH workflow.")
+
+print("Example:")
+
+url = "https://api.github.com/repos/octocat/hello-world/collaborators"
+headers = {
+        "Accept": "application/vnd.github.v3+json",
+        }
+response = requests.get(
+        url = url,
+        headers = headers
+        )
+
+datas = response.json()
+collaborators = {}
+
+for d in datas:
+    collaborator_name = d["login"]
+    collaborators[collaborator_name] = d["url"]
+
+print(collaborators)
