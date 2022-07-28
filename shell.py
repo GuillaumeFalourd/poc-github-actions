8f86ed764a9ede9780141fa33eb5f3e2d216ebc8
Guillaume Falourd <guillaume.falourd@zup.com.br>
2022-07-28 19:04:17 -0300

    test script
    Signed-off-by: Guillaume Falourd <guillaume.falourd@zup.com.br>

--- shell.py
+++ shell.py
@@ -1,15 +1,5 @@
 import subprocess
-import os
 
-try:
-    shell=os.environ['SHELL']
-except Exception:
-    shell = ""
-
-if 'bash' in shell:
-    result = subprocess.check_output("/c/windows/system32/wbem/wmic.exe csproduct get uuid", shell=False).strip().decode().split("\n")[1]
-else:
-    print(shell)
-    result = subprocess.check_output("c:\\windows\\system32\\wbem\\wmic.exe csproduct get uuid", shell=False).strip().decode().split("\n")[1]
+result = subprocess.check_output("c:\\windows\\system32\\wbem\\wmic.exe csproduct get uuid", shell=False).strip().decode().split("\n")[1] #nosec
 
 print(result)
