d97e83008065e93124a364fada902f8461010242
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2023-12-12 08:59:24 -0300

    update many-outputs python workflow

--- output_function.py
+++ output_function.py
@@ -1,8 +1,8 @@
 import os
 
 def save_output(name: str, value: str): 
-    with open(os.getenv('GITHUB_OUTPUT'), "a") as output_file:
-        output_file.write(f"{name}={value}")
+    with open(os.getenv['GITHUB_OUTPUT'], "a") as output_file:
+        print(f"{name}={value}", file=output_file)
 
 save_output("TEST1", "VALUE1")
 save_output("TEST2", "VALUE2")
