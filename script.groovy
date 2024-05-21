fd98d50113e1192ee79635c31168b060177029ee
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2024-05-21 16:19:57 -0300

    update workflow-tester96

--- script.groovy
+++ script.groovy
@@ -1,7 +1,25 @@
-#!groovy
+#!/usr/bin/env groovy
 
-sh 'cat Config.json'
-def configs = readJSON(file: 'Config.json')
+import groovy.json.JsonSlurper
+
+// Read the JSON file
+def jsonFile = new File('Config.json')
+def jsonSlurper = new JsonSlurper()
+def configs = jsonSlurper.parse(jsonFile)
+
+// Calculate the total number of bundles
 def totalBundles = configs.bundles.size()
-println "Total Array Size =" + totalBundles
+println "Total Array Size = " + totalBundles
+
+// Initialize an array for product names
 def productName = new Object[totalBundles]
+
+// Example to fill the array (assuming 'bundles' contains objects with a 'name' property)
+configs.bundles.eachWithIndex { bundle, index ->
+    productName[index] = bundle.product.name
+}
+
+// Print the product names
+productName.each { name ->
+    println "Product Name: " + name
+}
